53c86b424f87816b3080b386d8e1cd95e62fab63
Mikhail Komlev <58232010+atadan04@users.noreply.github.com>
2020-01-09 22:50:07 +0300

    Add new project: BankAccount

---
+++ BankAccount2/BankAccount2/BankAccount.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount2
+{
+    class BankAccount
+    {
+        private List<string> userInfo = new List<string>();
+        private bool status = false;
+        private decimal balance = 0;
+        DateTime createDate;
+        public BankAccount(string firstName, string lastName, string midName)
+        {
+            userInfo.Add(firstName);
+            userInfo.Add(lastName);
+            userInfo.Add(midName);
+            createDate = DateTime.Now;
+
+        }
+        public DateTime CreateDate
+            {get; }
+
+        public bool Status
+        {
+            get { return this.status; }
+            
+        }
+        public decimal Balance
+        { get; }
+        public void ActivateAccount()
+        {
+            if (this.status == true)
+            {
+                Console.WriteLine("Чувак, ты упоролся?Твоя сраная карта активна(ты лох)!");
+            }
+            else
+            {
+                this.status = true;
+            }
+        }
+        public void BlockedAccount()
+        {
+            if (this.status == false)
+            {
+                Console.WriteLine("Чувак, ты упоролся?Твоя сраная карта уже заблокирована!");
+            }
+            else
+            {
+                this.status = false;
+            }
+        }
+        public void Deposit(decimal countCash)
+        {
+            if(countCash<=0)
+            {
+                Console.WriteLine("Неверно указана сумма:(лошара");
+                
+            }
+            else
+            {
+                balance += countCash;
+                
+            }
+            
+        }
+
+        public void Spending(decimal countSpandingCash)//расход средств
+        {
+            if (this.Status == false)//проверка на блокировку
+            {
+                Console.WriteLine("Аккаунт заблокирован!");
+
+            }
+
+            else
+            {
+                if (countSpandingCash > balance)
+                {
+                    Console.WriteLine("Недостаточно средств на счете, пожалуйста пополните счет.");
+                }
+                else
+                {
+                    balance -= countSpandingCash;
+
+                }
+            }
+        }
+    }
+}
---
+++ BankAccount2/BankAccount2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount("1", "2", "3");
+            bankAccount.ActivateAccount();
+            
+            
+        }
+    }
+}
