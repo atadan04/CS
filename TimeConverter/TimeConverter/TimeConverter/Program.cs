94cf2e5c1608f5f87d8f9a97540441305ac245d8
Mikhail Komlev <58232010+atadan04@users.noreply.github.com>
2020-01-20 00:22:28 +0300

    Create new project TimeConverter

---
+++ TimeConverter/TimeConverter/TimeConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Second second = new Second();
+            second.Sec = 120;
+            Timer timer = (Timer)second;
+            Console.WriteLine($"{timer.Hou} : {timer.Min} : {timer.Sec}");
+
+            Second second1 = (Second)timer;
+            Console.WriteLine(second1.Sec);
+
+            Console.ReadLine();
+
+
+        }
+    }
+    
+
+}
+
---
+++ TimeConverter/TimeConverter/TimeConverter/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConverter
+{
+    class Timer
+    {
+        private int hou;
+        private int min;
+        private int sec;
+        public int Hou
+        {
+            get { return hou; }
+            set { hou = value; }
+        }
+        public int Sec
+        {
+            get { return sec; }
+            set { sec = value; }
+        }
+        public int Min
+        {
+            get { return min; }
+            set { min = value; }
+        }
+    }
+}
