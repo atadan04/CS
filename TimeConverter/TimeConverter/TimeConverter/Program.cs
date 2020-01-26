cb9f106165c51be2b8255e5f143104405dac9f7d
Mikhail Komlev <58232010+atadan04@users.noreply.github.com>
2020-01-26 18:46:02 +0300

    Updated version

--- TimeConverter/TimeConverter/TimeConverter/Program.cs
+++ TimeConverter/TimeConverter/TimeConverter/Program.cs
@@ -10,13 +10,38 @@ namespace TimeConverter
     {
         static void Main(string[] args)
         {
-            Second second = new Second();
-            second.Sec = 120;
-            Timer timer = (Timer)second;
-            Console.WriteLine($"{timer.Hou} : {timer.Min} : {timer.Sec}");
-            Second second1 = (Second)timer;
-            Console.WriteLine(second1.Sec);
-            Console.ReadLine();
+            Console.WriteLine("To convert seconds to Hou: Min: Sec format, enter the number of seconds");
+            Console.WriteLine("To convert from the format Hou: Min: Sec to seconds, enter  the time in the format Hou: Min: Sec ");
+            Timer timer = new Timer();
+            string output = Console.ReadLine();
+            string[] time = timer.Input(output);
+            if (output.Contains(":"))
+            {
+                timer.ConvertingToSec(time);
+                if (Timer.flag)
+                {
+                    timer.DisplaySec();
+                }
+
+            }
+            else
+            {
+
+                timer.ConvertingToTime(time);
+                if (Timer.flag)
+                {
+                    timer.DisplayTime();
+                }
+
+
+            }
+
+
+
+
+
+
+            Console.Read();
 
 
         }
--- TimeConverter/TimeConverter/TimeConverter/Timer.cs
+++ TimeConverter/TimeConverter/TimeConverter/Timer.cs
@@ -8,23 +8,94 @@ namespace TimeConverter
 {
     class Timer
     {
-        private int hou;
-        private int min;
-        private int sec;
-        public int Hou
+        private string[] Time;
+        public int Hour { get; set; }
+        public int Min { get; set; }
+        public int Sec { get; set; }
+        public static bool flag = true;
+        public void ConvertingToTime(string[] vs)
         {
-            get { return hou; }
-            set { hou = value; }
+            try
+            {
+                int sec = Convert.ToInt32(vs[0]);
+                if (sec >= 0)
+                {
+                    Hour = sec / 3600;
+                    Min = (sec - (Hour * 3600)) / 60;
+                    Sec = sec - ((Min * 60) + (Hour * 3600));
+                }
+                else
+                {
+
+                    flag = false;
+                    Console.WriteLine("Incorrect input!");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input! Please enter numbers (do not forget that hours, minutes and seconds are separated \":\")!");
+                flag = false;
+
+            }
+
+
+
         }
-        public int Sec
+        public void DisplayTime()
         {
-            get { return sec; }
-            set { sec = value; }
+            Console.WriteLine($"{Hour}:{Min}:{Sec}");
         }
-        public int Min
+        public void ConvertingToSec(string[] vs)
+
         {
-            get { return min; }
-            set { min = value; }
+            try
+            {
+                Hour = Convert.ToInt32(vs[0]);
+                Min = Convert.ToInt32(vs[1]);
+                Sec = Convert.ToInt32(vs[2]);
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Invalid input! Please enter numbers (do not forget that hours, minutes and seconds are separated \":\")!");
+                flag = false;
+            }
+
+
+            if (Hour >= 0 && Min >= 0 && Sec >= 0)
+            {
+                Sec = (Hour * 3600) + (Min * 60) + Sec;
+                Hour = 0;
+                Min = 0;
+            }
+            else
+            {
+                flag = false;
+                Console.WriteLine("Incorrect input!");
+            }
+
+
+
+
+
+
+        }
+        public void DisplaySec()
+        {
+            Console.WriteLine(Sec);
+        }
+        public string[] Input(string str)
+        {
+            if (str.Contains(":"))
+            {
+                return Time = str.Split(':');
+
+            }
+            else
+            {
+
+                return Time = new string[] { str };
+            }
         }
     }
 }
