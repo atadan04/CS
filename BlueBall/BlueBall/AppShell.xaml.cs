1d424e29fb4de4cc27ee94c19dde1d27610db24b
Mikhail Komlev <58232010+atadan04@users.noreply.github.com>
2020-02-10 23:18:05 +0300

    Add new project: BlueBall

---
+++ BlueBall/BlueBall/AppShell.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BlueBall
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+        private async void MenuItem_Clicked(object sender,System.EventArgs e)
+        {
+            await DisplayAlert("", "Привет!", "OK");
+            
+        }
+    }
+}
---
+++ BlueBall/BlueBall/Products.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BlueBall
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Products : ContentPage
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+    }
+}
